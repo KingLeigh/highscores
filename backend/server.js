f5fdad4f50490504f13e4b93fc67d4e8da2d3c4f
Leigh Herbert <leigh.herbert@gmail.com>
2020-12-24 13:11:08 -0500

    Removing extra redirect stuff for prod config

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
 }
 
 app.use(cors());
