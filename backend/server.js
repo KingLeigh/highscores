2a8a6b2103aab5a052005c638f59a417b15c793e
Leigh Herbert <leigh.herbert@gmail.com>
2020-12-24 13:29:38 -0500

    More fixes, closing in on a solution! Server currently works within React, but redirects break out of it.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production"){
+// if (process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
-}
+// }
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +30,12 @@ const scoresRouter = require('./routes/scores');
 app.use('/api/users', usersRouter);
 app.use('/api/events', eventsRouter);
 app.use('/api/scores', scoresRouter);
+
+// if (process.env.NODE_ENV === "production"){
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });  
+// }
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
