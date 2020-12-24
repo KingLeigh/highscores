aeea7065e7515c7884cd9d255ef6f00627ab8197
Leigh Herbert <leigh.herbert@gmail.com>
2020-12-24 11:33:14 -0500

    Removing prod server config - no evidence this helps YET

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-}
-
 app.use(cors());
 app.use(express.json());
 
