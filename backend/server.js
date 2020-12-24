ae533f2c38c8656c6ad2f90ad76ee704afff1bbf
Leigh Herbert <leigh.herbert@gmail.com>
2020-12-24 13:39:25 -0500

    Fixing file path for index.html on default route. This allows window.location navigation

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
-// }
+}
 
 app.use(cors());
 app.use(express.json());
@@ -31,11 +31,11 @@ app.use('/api/users', usersRouter);
 app.use('/api/events', eventsRouter);
 app.use('/api/scores', scoresRouter);
 
-// if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production"){
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "../build", "index.html"));
   });  
-// }
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
